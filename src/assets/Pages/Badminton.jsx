2a6887a4ffd28bc575b2fc23df796d6e641d8d49
devanterab <devan.terabayu81@gmail.com>
2023-06-12 16:53:05 +0700

    Update Badminton.jsx

--- src/assets/Pages/Badminton.jsx
+++ src/assets/Pages/Badminton.jsx
@@ -9,11 +9,20 @@ function Badminton() {
     <>
       <h1>Badminton Score Counter</h1>
       <div className="card">
-        <div>
-          <h1>Score A {count}</h1>
-        </div>
-        <div>
-          <h1>Score B {countB}</h1>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div style={{ marginRight: 30 }}>
+            <h1>Score A {count}</h1>
+          </div>
+          <div>
+            <h1>Score B {countB}</h1>
+          </div>
         </div>
         <button onClick={() => setCount((count) => count + 1)}>
           Point +1 for A
